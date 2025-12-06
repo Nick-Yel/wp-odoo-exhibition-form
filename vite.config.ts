d2acf9a4e9afb949edb46bf646e1ef17f7751b0d
Nick Yel Min Oo <nick_yel@royisal148.local>
2025-12-06 10:56:18 +0700

    change vite config for deploy purpose

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/wp-odoo-form/",
   build: {
     outDir: 'dist',
     emptyOutDir: true,
